b70444d0c5333eb1e3bb25043795c11f43b1b350
Cameron Cheung <cameroncheung@berkeley.edu>
2019-09-28 06:46:36 -0700

    Dot

--- dot.s
+++ dot.s
@@ -13,27 +13,35 @@
 #   a0 is the dot product of v0 and v1
 # =======================================================
 dot:
-
     # Prologue
+    addi sp sp -12
+    sw s0 0(sp)
+    sw s1 4(sp)
+    sw s2 8(sp)
 
+    li t0 0                 # t0: Sum
+    mv s2 a2
+    mv t3 a0                # t3: Curr index of v0
+    mv t4 a1                # t4: Curr index of v1
 
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
+    bge x0 s2 loop_end
+    lw t1 0(t3)             # t1: Value at curr v0 index
+    lw t2 0(t4)             # t2: Value at curr v1 index
+    mul t5 t1 t2            # t5: 2-Element Product = t1 * t2
+    add t0 t0 t5            # Sum += Product
+    add t3 t3 a3            # t3++
+    add t4 t4 a4            # t4++
+    addi s2 s2 -1           # s2--
+    j loop_start
 
 loop_end:
-
+    mv a0 t0
 
     # Epilogue
+    lw s0 0(sp)
+    lw s1 4(sp)
+    lw s2 8(sp)
+    addi sp sp 12
 
-    
     ret
--- test_files/test_dot.s
+++ test_files/test_dot.s
@@ -15,20 +15,22 @@ main:
     la s1 vector1
 
     # Set vector attributes
-    
-
+    li a2 9
+    li a3 4
+    li a4 4
 
     # Call dot function
-    
+    mv a0 s0
+    mv a1 s1
+    jal ra dot
 
-
-    # Print integer result
-
-
+    # Print the output of argmax
+    mv a1 a0
+    jal ra print_int
 
     # Print newline
-
-
+    li a1 '\n'
+    jal ra print_char
 
     # Exit
     jal exit
