84e3fabc7f2870fbe7f72365f6e6d3f25104599e
Cameron Cheung <cameroncheung@berkeley.edu>
2019-10-08 18:29:55 -0700

    A

--- test_files/test_write_matrix.s
+++ test_files/test_write_matrix.s
@@ -8,7 +8,11 @@ file_path: .asciiz "test_output.bin"
 .text
 main:
     # Write the matrix to a file
-
+    la a0 file_path
+    la a1 m0
+    li a2 3
+    li a3 3
+    jal write_matrix
 
     # Exit the program
     addi a0 x0 10
--- write_matrix.s
+++ write_matrix.s
@@ -20,21 +20,96 @@
 write_matrix:
 
     # Prologue
+    addi sp sp -28
+		sw s0 0(sp)
+		sw s1 4(sp)
+		sw s2 8(sp)
+		sw s3 12(sp)
+		sw s4 16(sp)
+		sw s5 20(sp)
+		sw ra 24(sp)
 
+		mv s0 a0                     # s0 = Filename
+		mv s1 a1                     # s1 = Pointer to Matrix
+		mv s2 a2                     # s2 = NumRows
+    mv s3 a3                     # s3 = NumCols
+    li a0 8
+    jal malloc                 # Store in a place
+    sw s2 0(a0)
+    sw s3 4(a0)
+    mv s5 a0
 
+    lw a1 0(s5)
+    jal print_int
+    lw a1 4(s5)
+    jal print_int
 
+		# Prepare for fopen
+		mv a1 s0                     # fopen Arg1: Filename
+		li a2 1                      # fopen Arg2: Write Permission
+		jal fopen
+		mv s4 a0                     # s4 = File Descriptor
 
+    mv a1 s4                     # Write number of rows
+    mv a2 s5                     #
+    li a3 1                      # Read 1 thing
+    li a4 4                      # Size of thing
+    jal fwrite
+    blt a0 a3 eof_or_error
+    addi s5 s5 4
 
+    mv a1 s4                     # Write number of columns
+    mv a2 s5                   #
+    li a3 1                      # Read 1 thing
+    li a4 4                      # Size of thing
+    jal fwrite
+    blt a0 a3 eof_or_error
 
+    li t0 0                     # Counter
+    mv t1 a1                    # Buffer to read from
+    mul s3 s2 s3                # Number of elems
 
+    li a1 'a'
+    jal print_char
 
+    mv a1 s4                    # Write number of columns
+    mv a2 s1                      #
+    mv a3 s3                    # Read s3 things
+    li a4 4                     # Size of thing
+    jal fwrite
+    blt a0 a3 eof_or_error
+    li a1 'a'
+    jal print_char
+    j end
+loop:
+    bge t0 s3 end
+    mv a1 s4                    # Counter
+    mv a2 t1                    # Buffer to read from
+    li a3 1                     # Nunber of elems
+    li a4 4                     # Size of thing
+    jal fwrite
+    blt a0 a3 eof_or_error
 
+    addi t0 t0 1
+    addi t1 t1 4
+    j loop
+
+end:
+    mv a1 s4                     # fclose Arg1: File Descriptor
+    jal fclose
+    bne a0 x0 eof_or_error       # Unsuccessful fclose
     # Epilogue
-
+    lw s0 0(sp)
+		lw s1 4(sp)
+		lw s2 8(sp)
+		lw s3 12(sp)
+		lw s4 16(sp)
+		lw s5 20(sp)
+		lw ra 24(sp)
+		addi sp sp 28
 
     ret
 
 eof_or_error:
     li a1 1
     jal exit2
-    
